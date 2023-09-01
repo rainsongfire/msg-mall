313f3c0bdf9e88c018de068b3de447e88149969c
RainsongFire <derui.song@gmail>
2023-09-01 22:47:03 +0800

    the third commit, all settings

---
+++ mall-activitysms/src/main/java/com/msb/mall/activitysms/MallActivitysmsApplication.java
@@ -0,0 +1,13 @@
+package com.msb.mall.activitysms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallActivitysmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallActivitysmsApplication.class, args);
+    }
+
+}
---
+++ mall-activitysms/src/test/java/com/msb/mall/activitysms/MallActivitysmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msb.mall.activitysms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallActivitysmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
